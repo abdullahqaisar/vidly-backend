570ccb1747a8af12c7a95b0fd973cafdfbbe2801
abdullahqaisar <abdullahqaisarr@gmail.com>
2023-08-07 05:26:22 -0400

    First commit

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+    email: Joi.string().min(5).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User; 
+exports.validate = validateUser;
